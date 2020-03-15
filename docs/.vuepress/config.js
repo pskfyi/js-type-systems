4f332f4f7188e11b5656d1a73b33b2ea9795a231
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2020-03-14 21:59:02 -0700

    feat(docs): init vuepress site

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'js-type-systems',
+  description: 'Docs for js-type-systems',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'icon', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'icon', href: '/favicon-32x32.png' }]
+  ]
+}
